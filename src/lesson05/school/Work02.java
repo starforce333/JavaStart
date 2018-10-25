0713caaf326d3870acbaecd3b61b0fb62c6577e7
starforce333 <starforce@i.ua>
2018-10-25 17:13:16 +0300

    Загрузил 5 урок

--- src/lesson05/school/Work02.java
+++ src/lesson05/school/Work02.java
@@ -10,12 +10,12 @@ public class Work02 {
         int[] pay = new int[12];
 
         for (int i = 0; i < pay.length; i++) {
-            pay[i] = 10000 + rn.nextInt(10000);      /////??????????
+            pay[i] = 10000 + rn.nextInt(10000);
         }
         System.out.println(Arrays.toString(pay));
 
         int sum = 0;
-        for (int i = 0; i < pay.length; i++) {       ////?????????????
+        for (int i = 0; i < pay.length; i++) {
             sum = sum + pay[i];
         }
 
