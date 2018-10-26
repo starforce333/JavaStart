b006d0ef5b0ca24232ac0491d330f41a652b179e
starforce333 <starforce@i.ua>
2018-10-26 17:59:27 +0300

    1 задание к 5 уроку

--- src/lesson06/home/Task01.java
+++ src/lesson06/home/Task01.java
@@ -1,15 +1,20 @@
 package lesson06.home;
 
 public class Task01 {
+    public static void main(String[] args) {
 
-int findMax (int array)
+        int[] array = new int[]{5, 4, 34, 7, 5, 3, 2, 7, 0, 8, 4, 324, 4, 69, 1,};
+        int max = findMax(array);
+        System.out.println(max);
+    }
 
-    String count (int a, int v, double c)
-
-        void draw (int h, int w)
-
-            int findNum (int array, int element)
-
-                int words (String text)
-                    //разделитель "Пробел"
+    public static int findMax(int[] a) {
+        int max = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] > max) {
+                max = a[i];
+            }
+        }
+        return max;
+    }
 }
---
+++ src/lesson06/home/Task02.java
@@ -0,0 +1,21 @@
+package lesson06.home;
+
+public class Task02 {
+    public static void main(String[] args) {
+
+    }
+
+
+}
+
+
+//
+//
+//    String count (int a, int v, double c)
+//
+//    void draw (int h, int w)
+//
+//    int findNum (int array, int element)
+//
+//    int words (String text)
+////разделитель "Пробел"
