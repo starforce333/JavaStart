1682428375b61db848dc14142fd38f6c6a605af0
starforce333 <starforce@i.ua>
2018-11-05 17:55:39 +0200

    Загрузил 8 занятие

--- src/lesson08/home/Task06.java
+++ src/lesson08/home/Task06.java
@@ -1,15 +1,56 @@
 package lesson08.home;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Task06 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter h");
+        int h = sc.nextInt();
+
+        System.out.println("Enter w");
+        int w = sc.nextInt();
+
+
+        String str = draw(h, w);
+
+        File file = new File("draw.txt");
+
+        try (PrintWriter pw = new PrintWriter(file)) {
+            pw.println(str);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private static String draw(int a, int b) {
+        String temp = "";
+        for (int i = 1; i <= a; i++) {
+            for (int j = 1; j <= b; j++) {
+                Random r = new Random();
+                if (j % 2 == 0) {
+                    char c = (char) (r.nextInt(26) + 'a');
+                    System.out.print(c);
+                    temp += c;
+                } else {
+                    char c = (char) (r.nextInt(26) + 'A');
+                    System.out.print(c);
+                    temp += c;
+                }
+            }
+            System.out.println();
+            temp += System.lineSeparator();
+        }
+        return temp;
+    }
 }
-
-
-
-
-
-
-
-
 
 
 //        1) Напишите метод для считывания двухмерного массива из
